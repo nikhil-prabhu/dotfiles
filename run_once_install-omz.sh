fdc0d47427c41b9888d0e3fb757cf8e5ab9b37da
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2026-03-19 22:42:03 +0100

    add oh my zsh init script

---
+++ run_once_install-omz.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install Oh My Zsh if it's not already there
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "Installing Oh My Zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+  echo "Oh My Zsh is already installed, skipping."
+fi
+
